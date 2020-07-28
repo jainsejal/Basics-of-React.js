8f00d71e2654c6b7c0043c9559ea2d2b8275d6e1
jainsejal <sejaljain1194@gmail.com>
2020-07-28 21:04:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 //Create a react app from scratch.
 import React from "react";
 import ReactDOM from "react-dom";
+
 const name = "Sejal Jain";
 const currentYear = new Date().getFullYear();
+const img = "https://picsum.photos/200";
+
 //It should display a h1 heading.
 ReactDOM.render(
   <div>
@@ -15,6 +18,12 @@ ReactDOM.render(
     </ul>
     <p> My lucky number is {Math.round(Math.random() * 10)}</p>
     <p>Copyright {currentYear}.</p>
+    <div>
+      <img src = {img + "?grayscale"} />
+      <img className = "food-img" src= "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSIfjkgzuJVB53eSgj_hunTHm39oHmA9wvR5g&usqp=CAU"/>
+      <img className = "food-img" src= "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRw-p9ekj_18G4A-SmlTjJYqjHnVwqjaT8zqA&usqp=CAU"/> 
+      <img className = "food-img" src= "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQP8GyC1-6RwZkF72eU2SItNmYOAKFHAbxkcQ&usqp=CAU"/>
+    </div>
   </div>,
   document.getElementById("root");
 )
