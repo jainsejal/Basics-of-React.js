bf044e42f1721ba37702001c59f2abc9a119cb40
jainsejal <sejaljain1194@gmail.com>
2020-07-28 18:53:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 //Create a react app from scratch.
 import React from "react";
 import ReactDOM from "react-dom";
+const name = "Sejal";
 //It should display a h1 heading.
 ReactDOM.render(
   <div>
-    <h1> My favourite food items:</h1>
+    <h1>Hello {name}!</h1>
+    <h3> My favourite food items:</h3>
     <ul>
       <li> Pani Puri </li>
       <li> Noodles </li>
       <li> Pizza </li>
     </ul>
+    <p> My lucky number is {Math.round(Math.random() * 10)}</p>
   </div>,
   document.getElementById("root");
 )
