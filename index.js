9bdc7e821312863abefad4ceafe07fd0186aa1c8
jainsejal <sejaljain1194@gmail.com>
2020-07-28 19:25:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 //Create a react app from scratch.
 import React from "react";
 import ReactDOM from "react-dom";
-const fName = "Sejal";
-const lName = "Jain";
+const name = "Sejal Jain";
+const currentYear = new Date().getFullYear();
 //It should display a h1 heading.
 ReactDOM.render(
   <div>
-    <h1>Hello {fName} {lName}!</h1>
+    <h1>Created by {name}!</h1>
     <h3> My favourite food items:</h3>
     <ul>
       <li> Pani Puri </li>
@@ -14,6 +14,7 @@ ReactDOM.render(
       <li> Pizza </li>
     </ul>
     <p> My lucky number is {Math.round(Math.random() * 10)}</p>
+    <p>Copyright {currentYear}.</p>
   </div>,
   document.getElementById("root");
 )
