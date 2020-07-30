750812e00d9636c3538eba150e0d5619d0eed4c0
jainsejal <sejaljain1194@gmail.com>
2020-07-30 16:04:29 +0200

    ES6 import/export

--- math.js
+++ math.js
@@ -10,5 +10,21 @@ function TriplePi(){
   return pi * 3
 }
 
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+
+function subtract(n1, n2) {
+  return n1 - n2;
+}
+
+function divide(n1, n2) {
+  return n1 / n2;
+}
+
 export default pi;
-export {DoublePi, TriplePi};
+export {DoublePi, TriplePi, add, multiply, subtract, divide};
