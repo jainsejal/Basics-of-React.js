452f8c45216f30c981ca76f987ad3033806dcc02
jainsejal <sejaljain1194@gmail.com>
2020-08-12 16:27:18 +0200

    UseState hook

---
+++ React useState Hook/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+function App(){
+const [count, setCount] = useState(0)
+
+function increment(){
+  setCount(count + 1)
+}
+
+function decrement(){
+  setCount(count - 1)
+}
+
+
+return(
+  <div>
+    <h1>{count}</h1>
+    <button onClick = {increment}>+</button>
+    <button onClick = {decrement}>-</button>
+  </div>
+)
+}
+
+export default App;
+
+
